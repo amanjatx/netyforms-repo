990e686ae23f23f5de32bd62a004548efcb2394a
JasmeetWillFlyx <jasskaur.dha@gmail.com>
2020-12-20 15:01:06 +0530

    add text

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Hello</h1>
       <h1>Step 1 run deploy </h1>
       <h1>This should not be in master file</h1>
     </div>
