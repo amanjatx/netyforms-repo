c5c484231cb9b791b091c4e2c65d8640652e7262
Aman Jat <amanjat261@gmail.com>
2020-12-20 15:37:40 +0530

    commit from xdummy

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1> hello from xdummy</h1>
       <h1>Step 1 run deploy </h1>
       <h1>This should not be in master file</h1>
     </div>
