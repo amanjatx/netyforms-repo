a75ebcb36169688531d418cd300dac24e5629a3e
xdummy <amanjat261@hotmail.com>
2020-12-20 18:00:27 +0530

    for educational

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> hello jiy</h1>
+      <h1> hello aman </h1>
       <h1>Step 1 run deploy </h1>
       <h1>This should not be in master file a</h1>
     </div>
