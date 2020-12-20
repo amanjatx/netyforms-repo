f9686ecf6617e5f533339be13d653db6f4aadad2
Aman Jat <amanjat261@gmail.com>
2020-12-20 17:42:15 +0530

    commit kar na yrr

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1> hello from xdummy</h1>
       <h1>Step 1 run deploy </h1>
-      <h1>This should not be in master file</h1>
+      <h1>This should not be in master file a</h1>
     </div>
   );
 }
