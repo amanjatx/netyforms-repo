b797e6976d304bf66725966c5b882057caa3dc79
JasmeetWillFlyx <jasskaur.dha@gmail.com>
2020-12-20 20:00:00 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hii I am jasmeet</h1>
       <h1>Step 1 run deploy </h1>
       <h1>This should not be in master file</h1>
     </div>
