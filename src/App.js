82029f75933e4a37af7574d1e6c87e77afa3a44c
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-22 10:27:42 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-
+import Header from './Component/Header.js';
+import Content from './Component/Content.js';
+import Footer from './Component/Footer.js';
 function App() {
   return (
     <div className="App">
-      <h1> hellokkkaman hi </h1>
-      <h1>Step 1 run deploy </h1>
-      <h1>This should not be in master file a</h1>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Component/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../CSS/style.module.css';
+export default function Header() {
+    return (
+        <div className={styles.headerClass}>
+            <h1>This is Header</h1>
+            <p>(Working area - jass)</p>
+        </div>
+    )
+}
