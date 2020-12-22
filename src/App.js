82a78d06db93fcb9fbdc8df5690147e2ec2165e8
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-22 19:19:34 +0530

    change made by me please accept for master branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1> hello i am jasmeet </h1>
-      <h1>Step 1 run deploy </h1>
+      <h1>Step 1 run deploy </h1> {/* this is a comment*/}
       <h1>This should not be in master file a</h1>
     </div>
   );
