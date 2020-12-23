6adab650279e7a8dcbdf6ce3c0a16790b5ba818e
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-23 21:00:42 +0530

    adding new line

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1> hello i am jasmeet </h1>
       <h1>Step 1 run deploy </h1> {/* this is a comment*/}
       <h1>This should not be in master file a</h1>
+      <h1>Adding line for amanxdummy</h1>
     </div>
   );
 }
