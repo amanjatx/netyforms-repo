5d4ca75931c6ccf8882fb9a67a025aa4f420826a
JasmeetWillFlyx <jasskaur.dha@gmail.com>
2020-12-30 20:25:37 +0530

    change header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from '../CSS/style.module.css';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import Logo from './logo';
+import ProfilePhoto from "./profilePhoto";
+
 export default function Header() {
     return (
-        <div className={styles.headerClass}>
-            <h1>This is Header</h1>
-            <p>(Working area - jass)</p>
-        </div>
+            
+             <div className={styles.headerClass}>
+                 <span className={styles.menuBar}><MenuRoundedIcon /></span>
+                 <Logo />
+                 <ProfilePhoto />
+             </div>
     )
     }
+
