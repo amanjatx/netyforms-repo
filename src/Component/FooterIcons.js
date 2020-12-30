f10f6f505363e800e61db459ae85c0afaf615879
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-30 13:04:47 +0530

    updating gh-pages

---
+++ src/Component/FooterIcons.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '../CSS/style.module.css';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+function FooterIcons() {
+    return (
+        <div className={styles.flexContainerIcons}>
+            <div><FacebookIcon /></div>
+            <div><InstagramIcon /></div>
+            <div><TwitterIcon /></div>
+            <div><YouTubeIcon /></div>
+            <div><LinkedInIcon /></div>
+        </div>
+    )
+}
+
+export default FooterIcons
---
+++ src/Component/FooterLinks.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FooterLinks() {
+    return (
+        <div>
+                        <p>About Us</p>
+            <p>Contact Us</p>
+            <p>Privacy Policy</p>
+        </div>
+    )
+}
+
+export default FooterLinks
