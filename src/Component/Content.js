23ac3c858fc1c81dcb4ad66e92b9923cb04c5bad
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-30 15:17:44 +0530

    updating background image

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from '../CSS/style.module.css';
+import background from '../Images/backimg.jpg';
+
+
+
 function Content() {
     return (
-        <div className={styles.contentClass}>
-            <div className={styles.ccquicklines}>
+        <div style={{ backgroundImage: `url(${background})` }} className={styles.contentClass}>
+            <div className={styles.ccquicklines}  style={{backgroundImage: "url('backimg.jpg')"
+              , backgroundRepeat: "no-repeat",
+              backgroundAttachment: "fixed",  
+              backgroundSize: "cover"
+        }}>
                 <p style={{fontSize : "25px"}}>Information is Wealth</p>
-                <p style={{fontSize : "15px"}}>Ask Anything, from anywhere</p>
+                <p style={{fontSize : "15px", fontFamily: "'Open Sans', sans-serif"}}>Ask Anything, from anywhere</p>
             </div>
         </div>
     )
