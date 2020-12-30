f66e2b2ca4f00c01cb9c429fe3c3bf3cc1f64232
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-30 12:56:48 +0530

    added flex property

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import styles from '../CSS/style.module.css';
 import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FooterIcons from './FooterIcons.js'
+import FooterLinks from './FooterLinks.js'
 function Footer() {
     return (
         <div className={styles.footerClass}>
-            <FacebookIcon />
-            <p>About Us</p>
-            <p>Contact Us</p>
-            <p>Privacy Policy</p>
+            <FooterIcons />
+            <hr></hr> {/* for a grey horizontal line */}
+            <FooterLinks />
         </div>
     )
 }
