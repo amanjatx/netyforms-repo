95ebb67059151cba71b7fc97573b355242f6f973
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-30 21:22:58 +0530

    update

---
+++ src/Component/CCquicklines.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const ccQuickLinesDiv = {display: 'flex', flexDirection: 'column', paddingTop : '6%'}
+const ccQuickLine1 = { textAlign : "center", margin : '2px 0px',fontSize : "25px", fontFamily: "'Pacifico', cursive" }
+const ccQuickLine2 = { textAlign : "center", margin : '2px 0px',fontSize : "15px", fontFamily: "'Open Sans', sans-serif"}
+
+function CCquicklines() {
+    return (
+            <div style={ccQuickLinesDiv}>
+                <p style={ccQuickLine1} >Information is Wealth</p>
+                <p style={ccQuickLine2}>Ask Anything, from anywhere</p>
+            </div>
+    )
+}
+
+export default CCquicklines
--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,26 +1,18 @@
 import React from 'react'
-import styles from '../CSS/style.module.css';
+// import styles from '../CSS/style.module.css';
 import background from '../Images/backimg.jpg';
 import CCbuttons from './CCbuttons.js'
+import CCquicklines from './CCquicklines';
 
-const ccTop = {
+const ccTop = { 
     color : 'rgb(255, 255, 255)',
     backgroundImage: `url(${background})`
-}
-const ccQuickLines = {
-    // fontFamily: Pacifico', cursive',
-    /* background-color: rgb(166, 166, 250); */
-    display: 'flex',
-    flexDirection: 'column',
 }
 
 function Content() {
     return (
         <div style={ccTop} >
-            <div className={ccQuickLines}>
-                <p style={{fontSize : "25px"}}>Information is Wealth</p>
-                <p style={{fontSize : "15px", fontFamily: "'Open Sans', sans-serif"}}>Ask Anything, from anywhere</p>
-            </div>
+            <CCquicklines />
             <CCbuttons />
         </div>
     )
