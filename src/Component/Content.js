bf594461a6368e6e6c38580487d47dee4db1f199
xdummy <76277513+xdummy@users.noreply.github.com>
2020-12-30 15:34:56 +0530

    update

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -7,11 +7,7 @@ import background from '../Images/backimg.jpg';
 function Content() {
     return (
         <div style={{ backgroundImage: `url(${background})` }} className={styles.contentClass}>
-            <div className={styles.ccquicklines}  style={{backgroundImage: "url('backimg.jpg')"
-              , backgroundRepeat: "no-repeat",
-              backgroundAttachment: "fixed",  
-              backgroundSize: "cover"
-        }}>
+            <div className={styles.ccquicklines}>
                 <p style={{fontSize : "25px"}}>Information is Wealth</p>
                 <p style={{fontSize : "15px", fontFamily: "'Open Sans', sans-serif"}}>Ask Anything, from anywhere</p>
             </div>
