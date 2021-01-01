262bc7f165783e47e7e15ec8bb2c1a0bee61e12a
xdummy <76277513+xdummy@users.noreply.github.com>
2021-01-01 23:41:36 +0530

    added new file

---
+++ src/Component/ContentDetail.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const heading = {
+    fontSize : '150%',
+}
+const para = {
+    fontSize : '80%',
+    fontFamily: "'Poppins', sans-serif"
+}
+const cdDiv = {
+    // background : "linear-gradient(to left, #ffffff 56%, #ffff00 150%)",
+    backgroundColor :'#ffff00',
+    margin : '-20px',
+    padding : '50px',
+    paddingTop : '30px',
+    fontFamily: "'Montserrat', sans-serif",
+}
+function ContentDetail() {
+    return (
+        <div style={{margin : '0px'}}>
+            <div style={cdDiv}>
+                <h2 style={heading}>Create Polls</h2>
+                <p style={para}>Create and share your poll with your friends, students, customers and other people.
+                    Gather Information about what they like, get their opinion
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ContentDetail
