a3009e1bfebe2e79f4954045104614e030be4a53
xdummy <76277513+xdummy@users.noreply.github.com>
2021-01-01 23:39:10 +0530

    contact detail added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Content from './Component/Content.js';
 import Footer from './Component/Footer.js';
 import Header2 from './Component/Header2';
+import ContentDetail from './Component/ContentDetail';
 
 function App() {
   return (
     <div className="App">
       <Header2 />
       <Content />
+      <ContentDetail />
       <Footer />
     </div>
 
