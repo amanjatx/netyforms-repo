eb4d0a946cf53a607a8eabc0c78eda27a247b160
xdummy <76277513+xdummy@users.noreply.github.com>
2021-02-21 12:30:40 +0530

    jai ho

--- src/Component/TempBlogPage.js
+++ src/Component/TempBlogPage.js
@@ -2,31 +2,54 @@ import { Divider } from '@material-ui/core'
 import React from 'react';
 import styles from '../CSS/blog.module.css';
 import AccessTimeRoundedIcon from '@material-ui/icons/AccessTimeRounded';
+import ThumbUpAltRoundedIcon from '@material-ui/icons/ThumbUpAltRounded';
+import ReplyRoundedIcon from '@material-ui/icons/ReplyRounded';
+import image from '../Images/blogdp.jpg'
+import HourglassEmptyRoundedIcon from '@material-ui/icons/HourglassEmptyRounded';
 
 function TempBlogPage() {
     return (
         <div className={styles.blog}>
-            <span className={styles.upperArea}></span>
-            <span className={styles.path}>
-                <label>
+            <div className={styles.section1}>
+                <label className={styles.path}>
                     Home > Blog >
                 </label>
-            </span>
-            <h1 className={styles.heading}>How to Earn Money Online 2021</h1>
-            
+                <h1 className={styles.heading}>
+                    How to Earn Money Online 2021
+                </h1>
+            </div>
+
             <Divider></Divider>
-            <span className={styles.metaData}>
-                <label className={styles.authorMeta}>
+
+            
+            <div className={styles.metaData}>
+                <div className={styles.authorDp}>
+                    <img src={image} height="56" width='55' className={styles.authorDpImage} alt={'dp'}></img>
+                </div>
+
+                <div className={styles.authorMeta}>
                     <i>Aman Jat</i>
                     <i>01 January 2021</i>
                     <span>
-                        <span style={{backgroundColor : 'red'}}><AccessTimeRoundedIcon /></span>
-                        <span style={{backgroundColor : 'blue'}}> 2 min</span>
+                        <span className={styles.timelogo}><HourglassEmptyRoundedIcon fontSize='small'/></span>
+                        <label> 2 min</label>
                     </span>
-                </label>
-            </span>
-            <Divider></Divider>
-            <p>So, if you are looking to start making more money or if you want a new career path that lets you earn money from home, this list is especially for you.
+                </div>
+
+                <div className={styles.responseSection}>
+                    <div className={styles.likeDiv}>
+                        <div className={styles.icon}><ThumbUpAltRoundedIcon /></div>
+                        <div className={styles.count}>0</div>
+                    </div>
+                    <div className={styles.likeDiv}>
+                        <span className={styles.icon}><ReplyRoundedIcon className={styles.forwardIcon} /></span>
+                        <span className={styles.count}>0</span>
+                    </div>
+                </div>
+            </div>
+
+            <Divider></Divider> 
+            <p className={styles.para}>So, if you are looking to start making more money or if you want a new career path that lets you earn money from home, this list is especially for you.
                 Over the past year I have been able to interview several people who are making money working from home. They are doing their own thing, and they are rocking at life.
                 Hopefully, you had the chance to read their interviews on my blog, but in case you missed any of them, this post rounds them all up for you.
                 This round-up includes everything from selling on Amazon, teaching English, becoming a cooking instructor, flipping items for profits, and more! If you’re wondering “How can I earn money working from home?”, then this is for you.
