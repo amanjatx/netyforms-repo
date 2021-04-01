2b7a45bdbf9161331189bab0fe075f95b64b0ed4
xdummy <76277513+xdummy@users.noreply.github.com>
2021-04-01 11:14:03 +0530

    button added

--- src/Component/ButtonOutlined.js
+++ src/Component/ButtonOutlined.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import {Button} from '@material-ui/core'
-import styles from '../CSS/com.module.css'
-
+// import styles from '../CSS/com.module.css'
+ // don't know why but style from module is not working
+const object1 = {
+    margin : `20px 10px`,
+    textTransform : `none`,
+    fontFamily: `'Quicksand', sans-serif`,
+    border : '1px solid white',
+    color : 'white',
+}
 function ButtonOutlined(props) {
     return (
         <>
             <Button onClick={props.method} 
                     variant='outlined' 
-                    // classes={{root : classes.rootButton}} 
-                    className={styles.ButtonOutlined}
+                    style={object1}
                     title={props.title}> 
-                
-                {props.title}
-            
-            </Button>
+                        
+                        {props.title} </Button>
         </>
     )
 }
